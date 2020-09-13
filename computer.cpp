79098f66860aeaea3a17e6f422706c3077c56724
jothiswarar <68116539+jothiswarar@users.noreply.github.com>
2020-09-13 16:24:38 +0530

    ,  ,

--- computer.cpp
+++ computer.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 using namespace std;
+extern int a;
 int main()
 {
-    int a,b,c ;//using comma operator
+    int a, b,c ;//using comma operator
     b = 30;
     c = 30;a=b;
     cout << "the value of a:" << a;
