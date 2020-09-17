b14ab1c10c4135d08d36e9f6d1af0ce84ad782f6
jothiswarar <68116539+jothiswarar@users.noreply.github.com>
2020-09-17 15:58:22 +0530

    mybhj

--- computer.cpp
+++ computer.cpp
@@ -5,8 +5,8 @@ int main()
 {
     int a = 30, b;
     b = a; // giving a=b gives sematic error
-    cout << setw(25) << "the value of b "<<"="<<setw(4) << b << endl;
-    cin>>a;
+    cout << setw(25) << "the value of b "<< "=" << setw(4) << b << endl;
+    cin >> a;
     b = a + 2;
     cout << setw(25) << "value of b "<< "=" << setw(4) << b << endl;
     return 0;
