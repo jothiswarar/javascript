8769b0d7664d10c1549aec07de6ecf05dd17d57b
jothiswarar <68116539+jothiswarar@users.noreply.github.com>
2020-09-18 19:43:35 +0530

    switch to c

--- Hello.c
+++ Hello.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
+int open();
 int main()
 {
-    printf("\ahello world\n");
+    int a = open();
+    printf("%d", a);
+    return 0;
+}
+int open()
+{
+    enum open
+    {
+        close = 0,
+        open = 1,
+        no = 0,
+        yes = 1,
+        off = 0,
+        on = 1,
+    };
+    scanf("%s", open);
+    return open;
 }
--- computer.cpp
+++ computer.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
-#include <iomanip>
 using namespace std;
 int main()
 {
     int a = 30, b;
-    b = a; // giving a=b gives sematic error
-    cout << setw(25) << "the value of b "<< "=" << setw(4) << b << endl;
-    b = a + 2;
-    cout << setw(25) << "value of b "<< "=" << setw(4) << b << endl;
+    b = a; // gives syntax error
+    cout<< "value of b :" << a;
     return 0;
 }
