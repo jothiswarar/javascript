982caf90d283c22ec58fffac5bf8dedcc58f23fc
jothiswarar <68116539+jothiswarar@users.noreply.github.com>
2020-09-18 20:43:41 +0530

    char

--- poundsymbol.c
+++ poundsymbol.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include <conio.h>
 int main()
 {
     int c = '\xA3'; // Symbol for pounds sterling
     printf("Character: %c Code: %d\n", c, c);
+    c=getchar();
+    printf("%c",c);
+    getch();
     return 0;
 }
