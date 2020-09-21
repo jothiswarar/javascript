67f36e796a5df5b4f48c6f7423e26327312e2768
jothiswarar <68116539+jothiswarar@users.noreply.github.com>
2020-09-21 19:37:30 +0530

    l;earn to use file

--- struct.c
+++ struct.c
@@ -4,30 +4,31 @@ struct group_member
 {
     char name[25];
     char subject[100];
-} member;
+}group;
 void store_file(struct group_member member);
 void printmember_details(struct group_member *member)
 {
-    printf("%s\n", member->name);
-    printf("%s", member->subject);
+    puts( member->name);
+    puts( member->subject);
 }
 int main()
 {
-    char nameof[25], subjectcan[100];
-    printf("enter name of person:");
-    gets(nameof);
-    printf("enter all the subject he can read (use ,):");
-    gets(subjectcan);
-    strcpy(member.name, nameof);
-    strcpy(member.subject, subjectcan);
-    printmember_details(&member);
-    store_file(member);
+    store_file(group);
     return 0;
 }
 void store_file(struct group_member member){
     FILE *group_memberfile;
+    char nameof[25], subjectcan[100];
     group_memberfile=fopen("group_member.txt","w+");
-    //fprintf(member->name, group_memberfile );
-    fprintf(group_memberfile, "%s", member.subject);
+    printf("enter name of person:");
+    gets(nameof);
+    printf("enter all the subject he do very well (use ,):");
+    gets(subjectcan);
+    strcpy(member.name, nameof);
+    strcpy(member.subject, subjectcan);
+    printmember_details(&member);
+    fputs(member.name, group_memberfile);
+    fputs("\n", group_memberfile);
+    fputs(member.subject,group_memberfile);
     fclose(group_memberfile);
 }
